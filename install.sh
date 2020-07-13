456d481acb9c43e9be2a8c7ff9629051862ed195
Shadow Walker <66953516+shadowwalker005@users.noreply.github.com>
2020-07-14 01:36:33 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,10 @@ chmod 777 shadow
 mv shadow $PREFIX/bin
 cd ..
 rm -rf shadow
+clear
+echo
+echo " ~ Shadow Has Been Installed Successfully"
+echo " ~ Just Type shadow To Run The Script"
+echo
+read -p ' ~ Hit Enter To Run Now..." run
 shadow
